189f0e75df89e8faac62dbe16de7e0117eb5d3d8
Luis <diazdiaz121526>
2021-05-26 10:12:21 -0500

    loginfin

--- index.php
+++ index.php
@@ -2,40 +2,66 @@
 <html>
 <head>
 	<title>Login</title>
+	<link rel="preload" href="css/normalize.css" as="stylesheet">
+	<link rel="stylesheet" type="text/css" href="css/normalize.css">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link rel="icon"  href="img/favicon.ico">
 	<link rel="preload" href="css/bootstrap.min.css" as="styles">
 	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
 	<link rel="preconnect" href="https://fonts.gstatic.com">
-	<link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;700&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
 	<link rel="preload" href="css/estilo.css" as="styles">
-	<link rel="stylesheet" type="text/css" href="css/estilo.css">
-	<script src="js/jquer.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/estilo.css">	<script src="js/jquer.min.js"></script>
 </head>
 <script>
 $(document).ready(function(){
   $("body").hide().fadeIn(900);
 })
 </script>
-<body>
-	<div id="contenedor1">
-		<form method="POST" action="Logica/valida.php">
-			<div class="logo">
-			<div class="text-center">
-			  <img src="Img/LogoSicasa.png" class="img-fluid" alt="Logotipo">
-			</div>
-			</div>
-			<div class="form-group">
-		    <input type="text" class="form-control" name="user" aria-describedby="emailHelp" placeholder="Usuario">
-		  </div>
-		  <br>
-		  <div class="form-group">
-		    <input type="password" class="form-control" name="pass" placeholder="Contraseña">
-		  </div>
-		  <br>
-		  <button type="submit" class="btn btn-success">Accesar</button>
-		</form>
-	</div>
+<header class="he">
 
+ <h1>CorpoSupport </h1>
+ 
+  </header>
+<body>
+	<main >
+	<div class="contenedorindex">
+		<form method="POST" action="Logica/valida.php" >
+			<fieldset>
+				<legend>Bienvenido</legend>
+					<div>
+						<svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-affiliate" width="44" height="44" viewBox="0 0 24 24" stroke-width="3" stroke="#173686" fill="none" stroke-linecap="round" stroke-linejoin="round">
+						<path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+						<path d="M5.931 6.936l1.275 4.249m5.607 5.609l4.251 1.275" />
+						<path d="M11.683 12.317l5.759 -5.759" />
+						<circle cx="5.5" cy="5.5" r="1.5" />
+						<circle cx="18.5" cy="5.5" r="1.5" />
+						<circle cx="18.5" cy="18.5" r="1.5" />
+						<circle cx="8.5" cy="15.5" r="4.5" />
+						</svg>
+					</div>
+					<br>
+					<div>
+						<input type="text" name="user" placeholder="Usuario">
+					</div>
+					<br>
+					<div>
+						<input type="password" name="pass" placeholder="Contraseña">
+					</div>
+					<br>
+					<div>
+						<input type="submit" value="Accesar" class="btn btn-success">
+					</div>
+			</fieldset>
+		</form>
+		</div>
+	</main>
 </body>
+<footer class="footerindex"> 
+Copyright &reg; 2021-<script type="text/javascript">document.write(new Date().getFullYear());
+</script> CorpoSupport - Todos los derechos reservados.
+</footer>
 </html>
+
+
