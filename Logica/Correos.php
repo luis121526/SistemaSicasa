66dbf1a3df338c23befaa03491320f21c409c6e9
Luis <diazdiaz121526>
2021-06-04 17:56:11 -0500

    CORREO

--- Logica/Correos.php
+++ Logica/Correos.php
@@ -7,13 +7,6 @@ $info5=$_POST['telefono'];
 $info6=$_POST['asunto'];
 $info7=$_POST['problema'];
 
-echo "$info1";
-echo "$info2";
-echo "$info3";
-echo "$info4";
-echo "$info5";
-echo "$info6";
-echo "$info7";
 
 
 ?>
