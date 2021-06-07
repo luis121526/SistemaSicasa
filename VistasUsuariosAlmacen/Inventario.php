3f3e108757ccecd69ab8b08014d463e235635328
Luis <diazdiaz121526>
2021-06-07 11:18:35 -0500

    qw

--- VistasUsuariosAlmacen/Inventario.php
+++ VistasUsuariosAlmacen/Inventario.php
@@ -22,7 +22,7 @@ else{
     <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="../DataTables/datatables.min.js"/>
     <script type="text/javascript" src="../DataTables/datatables.min.js"></script>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
     <script src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
     <title>Dashboard</title>
