511d41ae7ffffc5b92e53320dacc552dab2301a6
Luis <diazdiaz121526>
2021-06-07 13:45:56 -0500

    sd

--- VistasUsuariosAlmacen/Ticket_Servicio.php
+++ VistasUsuariosAlmacen/Ticket_Servicio.php
@@ -1,7 +1,7 @@
 <?php   
 session_start();
-$usuario=$_SESSION['logueo'];
-if (!$usuario){
+$usuario1=$_SESSION['logueo'];
+if (!$usuario1){
     header("location:../index.php");
 }
 else{
@@ -42,7 +42,7 @@ else{
 
     <div class="contenedor3 col text-center">
        <!-- <a href="../Logica/cerrar.php">adios</a>-->
-       <span><?php echo "Hola $usuario" ?></span>
+       <span><?php echo "Hola $usuario1" ?></span>
     <span>
     <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-user" width="32" height="32" viewBox="0 0 24 24" stroke-width="2.5" stroke="#ffffff" fill="none" stroke-linecap="round" stroke-linejoin="round">
         <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
--- VistasUsuariosComun/Dashboard.php
+++ VistasUsuariosComun/Dashboard.php
@@ -12,11 +12,11 @@ else{
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="preload" href="../css/normalize.css" as="stylesheet">
+    <link rel="preload" href="../css/normalize.css" as="style">
     <link rel="stylesheet" href="../css/normalize.css">
-    <link rel="preload" href="../css/bootstrap.min.css" as="stylesheet">
+    <link rel="preload" href="../css/bootstrap.min.css" as="style">
     <link rel="stylesheet" href="../css/bootstrap.min.css">
-    <link rel="preload" href="../css/estilo.css" as="stylesheet">
+    <link rel="preload" href="../css/estilo.css" as="style">
     <link rel="stylesheet" href="../css/estilo.css">
     <link href="https://fonts.googleapis.com/css2?family=Pattaya&display=swap" rel="stylesheet">
     <script src="../js/jquer.min.js"></script>
